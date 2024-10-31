3fca89e876fcdb5396abec81a3a843eefc0d280c
Trekkateer <jcn6@calvin.edu>
2024-10-31 01:36:42 -0400

    Updated the scraper for Seychelles

--- scrapers/law-scrapers/SC.php
+++ scrapers/law-scrapers/SC.php
@@ -2,8 +2,8 @@
     <?php
         //Settings
         $test = true; $country = 'SC';
-        $start = 0;//Which year to start from
-        $limit = null;//Which year to end at
+        $start = 0;//Which page to start from
+        $limit = null;//Which page to end at
 
         //Opens the parser (HTML_DOM)
         include '../simple_html_dom.php'; // '../' refers to the parent directory
@@ -19,6 +19,28 @@
         $SQL1 = "TRUNCATE TABLE `dbpsjng5amkbcj`.`laws".strtolower($country)."`"; echo $SQL1.'<br/><br/>';
         if (!$test) {$conn->query($SQL1);}
 
+        //Sanitizes the data
+        $sanitizeName = [
+            '0f'=>'of', 'OF'=>'of', 'of 222'=>'of 2022', 'of 223'=>'of 2023',
+            'S.I.'=>'SI ', 'S.I'=>'SI ', 'S. I.'=>'SI ', 'S.1.'=>'SI ',
+            '(Bill. No'=>'(Bill No.', '(bill No.'=>'(Bill No.', '(Bill no.'=>'(Bill No.', '(bill no.'=>'(Bill No.',
+            '( '=>'(', ' )'=>')',
+            '  '=>' ',
+        ];
+        $sanitizeURL = [
+            'S.I.'=>'SI', ' of 20'=>'%2020', 'No.'=>'No%20', ', 20'=>'%2020',
+            ' '=>'%20', "'"=>'', '’'=>'%27',
+        ];
+
+        //Makes sure the ID is 3 digits long
+        $randNum = 0;
+        $zero_buffer = function ($inputNum, $outputLen=3) use (&$randNum) {
+            $outputNum = trim($inputNum);
+            if ($outputNum === '') {$outputNum = $randNum; $randNum++;}
+            while (strlen($outputNum) < $outputLen) {$outputNum = '0'.$outputNum;}
+            return $outputNum;
+        };
+
         //Gets the limit
         $html_dom = file_get_html('https://www.nationalassembly.sc/legislation');
         $limit = $limit ?? explode('page=', $html_dom->find('a[title="Go to last page"]')[0]->href)[1];
@@ -28,23 +50,25 @@
             $html_dom = file_get_html('https://www.nationalassembly.sc/legislation?type=All&search=&page='.$page);
             $laws = $html_dom->find('table.cols-4')[0]->find('tbody')[0]->find('tr');
             foreach ($laws as $law) {
-                //Gets the type
-                $type = strtr(trim($law->find('td')[1]->plaintext), array(' (S.I)'=>'')); $status = 'Valid';
-
-                //Gets values
-                $enactDate = date('Y/m/d', strtotime(trim(str_replace('/', '-', $law->find('td')[3]->plaintext)))); $enforceDate = $enactDate;
+                //Gets the name
+                $name = trim(strtr($law->find('td')[0]->plaintext, $sanitizeName));
+                //Gets the type and the rest of the values
+                $type = explode(' (', trim($law->find('td')[1]->plaintext))[0]; $status = 'Valid';
                 if ($type === 'Statutory Instrument') {
-                    $ID = $country.'-'.trim(strtr(explode(' - ', $law->find('td')[0]->plaintext)[0], array(' of '=>'', 'S.I. '=>'SI')));
-                    $name = trim(explode(' - ', $law->find('td')[0]->plaintext)[1]);
-                    $pdf = 'https://www.nationalassembly.sc/sites/default/files/'.date('Y-m', strtotime($enactDate)).'/'.strtr(trim(explode(', 20', $law->find('td')[0]->plaintext)[0]).', '.date('Y', strtotime($enactDate)), array('S.I.'=>'SI', ' of 20'=>' 20', 'No.'=>'No ', "'"=>'', 'Notice, 20'=>'Notice 20', 'Order, 20'=>'Order 20', 'Regulations, 20'=>'Regulations 20', '('=>'%28', ')'=>'%29')).'.pdf';
-                    if (str_contains($name, 'Amendment')) {$type = $type.' Amendment';}
-                } else {
-                    $ID = $country.'-B'.trim(strtr(explode(' (Bill No.', $law->find('td')[0]->plaintext)[1], array(' of '=>'', ')'=>'')));
-                    $name = trim(explode(' (Bill No.', $law->find('td')[0]->plaintext)[0]);
-                    $pdf = 'https://www.nationalassembly.sc/sites/default/files/'.date('Y-m', strtotime($enactDate)).'/Bill '.strtr(trim(explode(' (Bill No.', $law->find('td')[0]->plaintext)[1]), array(' of '=>' ', ')'=>'')).' - '.strtr(trim(explode(' (Bill No.', $law->find('td')[0]->plaintext)[0]), array(', 20'=>' 20', '('=>'%28', ')'=>'%29')).'.pdf';
-                    if (str_contains($name, 'Amendment')) {$type = $type.' Amendment';}
+                    $enactYear = end(explode(' ', trim(explode('-', $name)[0], ' )')));
+                    $ID = $country.'-SI'.$zero_buffer(explode(' of ', explode('SI ', $name)[1])[0]).$enactYear;
+                    //$PDF = 'https://www.nationalassembly.sc/sites/default/files/'.trim(explode('/', ($law->find('td')[3]->plaintext))[2]).'-'.explode('/', $law->find('td')[3]->plaintext)[1].'/'.strtr(trim(explode(', '.$enactYear, $name)[0]).', '.$enactYear, $sanitizeURL).'.pdf';
+                } else if ($type === 'Bill') {
+                    $enactYear = end(explode(' ', trim($name, ' )')));
+                    $ID = $country.'-B'.$zero_buffer(explode(' of ', trim(explode('(Bill No.', $name)[1]))[0]).$enactYear;
+                    //$PDF = 'https://www.nationalassembly.sc/sites/default/files/'.trim(explode('/', $law->find('td')[3]->plaintext)[2]).'-'.explode('/', $law->find('td')[3]->plaintext)[1].'/Bill '.strtr(trim(explode('(Bill No.', $name)[1]), array(' of '=>' ', ')'=>'')).' - '.strtr($name, $sanitizeURL).'.pdf';
                 }
+                if (str_contains($name, 'Amendment')) {$type = 'Amendment to '.$type;}
                 $source = 'https://www.nationalassembly.sc'.$law->find('td')[0]->find('a')[0]->href;
+                //Gets the regime
+                if (strtotime($enactYear.'-01-01') < strtotime('29 June 1976')) {
+                    $regime = 'The British Empire';
+                } else {$regime = 'The Republic of Seychelles';}
 
                 //Makes sure there are no quotes in the title
                 if (str_contains($name, "'")) {$name = str_replace("'", "’", $name);}
@@ -52,11 +76,11 @@
                 //JSONifies the values
                 $name = '{"en":"'.$name.'"}';
                 $source = '{"en":"'.$source.'"}';
-                $pdf = '{"en":"'.$pdf.'"}';
+                //$PDF = '{"en":"'.$PDF.'"}';
                 
                 //Inserts the new laws
-                $SQL2 = "INSERT INTO `laws".strtolower($country)."`(`enactDate`, `enforceDate`, `ID`, `name`, `type`, `status`, `source`, `pdf`) 
-                            VALUES ('".$enactDate."', '".$enforceDate."', '".$ID."', '".$name."', '".$type."', '".$status."', '".$source."', '".$pdf."')"; echo $SQL2.'<br/>';
+                $SQL2 = "INSERT INTO `laws".strtolower($country)."`(`enactDate`, `enforceDate`, `lastactDate`, `ID`, `regime`, `name`, `type`, `status`, `source`) 
+                            VALUES ('".$enactYear."-01-01', '".$enactYear."-01-01', '".$enactYear."-01-01', '".$ID."', '".$regime."', '".$name."', '".$type."', '".$status."', '".$source."')"; echo $SQL2.'<br/>';
                 if (!$test) {$conn->query($SQL2);}
             }
         }
