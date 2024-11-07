414ab2049838143f771fc3381bbde208c654f7df
Trekkateer <jcn6@calvin.edu>
2024-11-06 22:00:35 -0500

    Fixed scraper for Sri Lanka

--- scrapers/law-scrapers/LK.php
+++ scrapers/law-scrapers/LK.php
@@ -1,7 +1,9 @@
 <html><body>
-    <?php //!!There is no consistant ID !!Many laws are missing a source page
+    <?php //!!Many laws are missing a source page
+        //TODO: Find a way to get source pages
+        //TODO: Implement ammendment support
         //Settings
-        $test = true; $country = 'LK';
+        $test = false; $country = 'LK';
         $start = 0;//Which law to start from
         $step = 20;//How many laws are on each page
         $limit = null;//Total number of laws desired
@@ -46,7 +48,7 @@
         foreach (array('si', 'ta', 'en') as $lang) {
             //Gets the limit
             $html_dom->load($HTTP_Call($lang, 'https://parliament.lk/business-of-parliament/index2.php?option=com_actsandbills&task=acts&tmpl=component&start=0&legis=Please%20select%20a%20Legislature&year=&keyword=&id=undefined&type=0&actno='));
-            $limit = $limit ?? explode("'", end($html_dom->find('a.pagenav'))->onclick)[1]; echo $limit;
+            $limit = $limit ?? explode("'", end($html_dom->find('a.pagenav'))->onclick)[1]; echo $limit.'<br/>';
 
             //Loops through the pages
             for ($offset = $start; $offset <= $limit; $offset += $step) {
@@ -54,14 +56,38 @@
                 $html_dom->load($HTTP_Call($lang, 'https://parliament.lk/business-of-parliament/index2.php?option=com_actsandbills&task=acts&tmpl=component&start='.$offset.'&legis=Please%20select%20a%20Legislature&year=&keyword=&id=undefined&type=0&actno='));
                 $laws = $html_dom->find('div.acts_box');
                 foreach ($laws as $lawNum => $law) {//echo $law;
-                    $enactDate = str_replace('-', '/', explode(': ', $law->find('div.nTabber_content')[0]->find('div.con_box')[1]->plaintext)[1]); $enforceDate = $enactDate;
-                    $ID = $country.'-'.strtr(trim(explode(':', $law->find('div.acts_box_top')[0]->find('a.nTabber_plus')[0]->plaintext)[0]), array('/'=>'', 'Chap '=>'CHAP'));
+                    $enactDate = explode(': ', $law->find('div.nTabber_content')[0]->find('div.con_box')[1]->plaintext)[1]; $enforceDate = $enactDate; $lastActDate = $enactDate;
+                    $ID = $country.'-ACT'.strtr(trim(explode(':', $law->find('div.acts_box_top')[0]->find('a.nTabber_plus')[0]->plaintext)[0]), array('/'=>'', 'Chap '=>'CHAP'));
+                    //Gets the regime
+                    switch (true) {
+                        case (strtotime($enactDate) < strtotime('1978-02-04') ):
+                            $regime = 'The Democratic Socialist Republic of Sri Lanka';
+                        case (strtotime($enactDate) < strtotime('1948-02-04')):
+                            $regime = 'British Ceylon';
+                        case (strtotime($enactDate) < strtotime('1815-02-04')):
+                            $regime = 'The Kingdom of Kandy';
+                        default: $regime = 'Sri Lanka';
+                            break;
+                    }
+                    //Gets the rest of the values
                     $name = trim(explode(':', $law->find('div.acts_box_top')[0]->find('a.nTabber_plus')[0]->plaintext)[1]);
                     $type = 'Act';
-                        if (str_contains($name, 'සංශෝධන')) {$type = $type.' Amendment';}
-                        if (isset($law->find('img[src="https://parliament.lk/images/c-ammend.png"]')[0])) {$type = 'Constitutional Amendment'; $ID = 'LK-CON'.str_replace('/', '', $enactDate);}
+                        if (str_contains($name, 'සංශෝධන')) {$type = 'Amendment to '.$type;}
+                        $conAmmendNum = 0;
+                        if (isset($law->find('img[src="https://parliament.lk/images/c-ammend.png"]')[0])) {
+                            $conAmmendNum++;
+                            $type = 'Constitutional Amendment';
+                            $ID = 'LK-CON'.$conAmmendNum.explode('-', $enactDate)[0];
+                        }
                     $status = 'Valid';
-                    $source = $law->find('div.nTabber_content')[0]->find('div.con_box')[2]->find('a.act_down')[0]->href ?? 'NULL'; $PDF = $source;
+                    //Gets the source
+                    if (isset($law->find('div.nTabber_content')[0]->find('div.con_box')[2]->find('a.act_down')[0])) {
+                        $isSource = true;
+                        $source = $PDF = '"'.$law->find('div.nTabber_content')[0]->find('div.con_box')[2]->find('a.act_down')[0]->href.'"';
+                    } else {
+                        $isSource = false;
+                        $source = $PDF = null;
+                    }
 
                     //Makes sure there are no quotes in the title
                     if (str_contains($name, "'")) {$name = str_replace("'", "’", $name);}
@@ -78,12 +104,12 @@
 
                             //JSONifies the href
                             $compoundedSource = json_decode($row['source'], true);
-                            $compoundedSource[$lang] = $source;
+                            $compoundedSource[$lang] = trim($source, '"');
                             $source = json_encode($compoundedSource, JSON_UNESCAPED_UNICODE);
 
                             //JSONifies the PDF
                             $compoundedPDF = json_decode($row['PDF'], true);
-                            $compoundedPDF[$lang] = $PDF;
+                            $compoundedPDF[$lang] = trim($PDF, '"');
                             $PDF = json_encode($compoundedPDF, JSON_UNESCAPED_UNICODE);
 
                             //Creates SQL
@@ -92,17 +118,19 @@
                     } else {
                         //JSONifies the name and href
                         $name = '{"'.$lang.'":"'.$name.'"}';
-                        $source = '{"'.$lang.'":"'.$source.'"}';
-                        $PDF = '{"'.$lang.'":"'.$PDF.'"}';
+                        $source = '{"'.$lang.'":'.$source.'}';
+                        $PDF = '{"'.$lang.'":'.$PDF.'}';
 
                         //Creates SQL
-                        $SQL2 = "INSERT INTO `laws".strtolower($country)."`(`enactDate`, `enforceDate`, `ID`, `name`, `type`, `status`, `source`, `PDF`)
-                                VALUES ('".$enactDate."', '".$enforceDate."', '".$ID."', '".$name."', '".$type."', '".$status."', '".$source."', '".$PDF."')";
+                        $SQL2 = "INSERT INTO `laws".strtolower($country)."`(`enactDate`, `enforceDate`, `lastActDate`, `ID`, `regime`, `name`, `type`, `status`, `source`, `PDF`)
+                                VALUES ('".$enactDate."', '".$enforceDate."', '".$lastActDate."', '".$ID."', '".$regime."', '".$name."', '".$type."', '".$status."', '".$source."', '".$PDF."')";
                     }
 
                     //Makes the query
-                    echo $SQL2.'<br/>';
-                    if (!$test) {$conn->query($SQL2);}
+                    if ($isSource) {//Only executes if there is a source
+                        echo $SQL2.'<br/>';
+                        if (!$test) {$conn->query($SQL2);}
+                    }
                 }
             }
 
