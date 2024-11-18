f6dce506515d7c31fc0450c4c462e60fc61a0c93
Trekkateer <jcn6@calvin.edu>
2024-11-18 01:57:54 -0500

    Updated scraper for Tuvalu

--- scrapers/law-scrapers/TV.php
+++ scrapers/law-scrapers/TV.php
@@ -1,7 +1,7 @@
 <html><body>
     <?php
         //Settings
-        $test = true; $country = 'TV';
+        $test = false; $country = 'TV';
 
         //Opens the parser (HTML_DOM)
         include '../simple_html_dom.php'; // '../' refers to the parent directory
@@ -17,6 +17,10 @@
         //Clears the table
         $SQL1 = "TRUNCATE TABLE `dbpsjng5amkbcj`.`laws".strtolower($country)."`"; echo $SQL1.'<br/><br/>';
         if (!$test) {$conn->query($SQL1);}
+
+        //Makes sure certain things get capitalized TODO: Add more
+        $capitalizees    = [' british', ' the crown', ' the ekalesia a kelisiano', ' england', ' ireland', ' falekaupule', ' the multilateral investment guarantee agency', ' the international centre for the settlement of investment disputes', ' the international development association', ' international bank for reconstruction and development', ' the international finance corporation', ' international monetary fund', ' kaupule', ' red cross', ' the tuvalu philatelic bureau', ' the tuvalu telecommunications corporation', ' tuvalu'];
+        $capitalizations = [' British', ' the Crown', ' the Ekalesia a Kelisiano', ' England', ' Ireland', ' Falekaupule', ' the Multilateral Investment Guarantee Agency', ' the International Centre for the Settlement of Investment Disputes', ' the International Development Association', ' International Bank for Reconstruction and Development', ' the International Finance Corporation', ' International Monetary Fund', ' Kaupule', ' Red Cross', ' the Tuvalu Philatelic Bureau', ' the Tuvalu Telecommunications Corporation', ' Tuvalu'];
 
         //Loops through all the letters
         foreach (array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z') as $letter) {
@@ -52,18 +56,21 @@
                 //Gets raw values
                 $enactDate = $body_row->find('td')[5]->find('div')[0]->{'data-bs-content'};
                 $ID = $body_row->find('td')[1]->find('div')[0]->{'data-bs-content'};
-                $name = $body_row->find('td')[3]->find('a')[0]->innertext;
+                $name = $body_row->find('td')[3]->find('a')[0]->plaintext;
                 $summary = $body_row->find('td')[1]->find('div')[0]->{'data-bs-content'};
                 $topic = $body_row->find('td')[4]->find('div')[0]->{'data-bs-content'};
                 $source = $body_row->find('td')[3]->find('a')[0]->href;
                 
                 //Sanitizes some values
-                $enactDate = date('Y-m-d', strtotime($enactDate)); $enforceDate = $enactDate;
+                $enactDate = date('Y-m-d', strtotime($enactDate)); $enforceDate = $enactDate; $lastactDate = $enactDate;
                 $ID = $country.'-'.explode('<hr', explode(': ', $ID)[1])[0];
-                $name = explode('&nbsp', $name)[0];
-                $summary = explode("<hr class='notes'>", $summary)[1] ?? 'NULL';
-                    $summary = $summary === 'NULL' ? $summary:ucfirst(strtolower($summary));
-                $type = 'Act'; $status = 'Valid';
+                if (strtotime($enactDate) < strtotime('1 October 1978')) {$regime = '{"en":"The British Empire"}';} else {$regime = '{"en":"Tuvalu"}';}
+                $name = explode('&nbsp;', $name)[0];
+                $summary = strtolower(explode("<hr class='notes'>", $summary)[1] ?? 'NULL');
+                    $summary = ($summary !== 'null' && str_starts_with($summary, 'an act')) ? ucfirst(str_replace($capitalizees, $capitalizations, $summary)):'NULL';
+                $type = 'Act';
+                if (str_contains($name, 'Amendment')) {$isAmend = 1;} else {$isAmend = 0;}
+                $status = 'Valid';
                 $topic = trim(explode('<br></span>', explode("'>", $topic)[1])[0]);
                 $source = 'https://laws.bahamas.gov.bs'.$source;
 
@@ -79,8 +86,9 @@
                 $source = '{"en":"'.$source.'"}'; $PDF = $source;
                 
                 //Inserts the new laws
-                $SQL2 = "INSERT INTO `laws".strtolower($country)."`(`enactDate`, `enforceDate`, `ID`, `name`, `summary`, `type`, `topic`, `status`, `source`, `PDF`) 
-                            VALUES ('".$enactDate."', '".$enforceDate."', '".$ID."', '".$name."', ".$summary.", '".$type."', '".$topic."', '".$status."', '".$source."', '".$PDF."')"; echo $SQL2.'<br/>';
+                echo $summary.'<br/>';
+                $SQL2 = "INSERT INTO `laws".strtolower($country)."`(`enactDate`, `enforceDate`, `lastactDate`, `ID`, `regime`, `name`, `summary`, `type`, `isAmend`, `topic`, `status`, `source`, `PDF`) 
+                            VALUES ('".$enactDate."', '".$enforceDate."', '".$lastactDate."', '".$ID."', '".$regime."', '".$name."', ".$summary.", '".$type."', '".$isAmend."', '".$topic."', '".$status."', '".$source."', '".$PDF."')"; //echo $SQL2.'<br/>';
                 if (!$test) {$conn->query($SQL2);}
             }
         }
