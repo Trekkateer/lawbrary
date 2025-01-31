4d8930d3504c7b9d416e84f19e6ff221398e383f
Trekkateer <jcn6@calvin.edu>
2025-01-30 19:14:27 -0500

    Updated the way laws are stored in the database

--- scrapers/populateLawsDB.php
+++ scrapers/populateLawsDB.php
@@ -7,9 +7,7 @@
         //Updates the date on the countries table
         foreach(array('countries', 'divisions', 'divisions2', 'organizations') as $table) {
             //Connects to the content database
-            $username="ug0iy8zo9nryq";
-            $password="T_1&x+$|*N6F";
-            $database="dbupm726ysc0bg";
+            $username="ug0iy8zo9nryq"; $password="T_1&x+$|*N6F"; $database="dbupm726ysc0bg";
             $conn = new mysqli("localhost", $username, $password, $database);
             $conn->select_db($database) or die("Unable to select database");
 
@@ -18,18 +16,16 @@
 
             while($row = $result->fetch_assoc()) {
                 //Connects to the Law database
-                $username2="u9vdpg8vw9h2e";
-                $password2="f1x.A1pgN[BwX4[t";
-                $database2="dbpsjng5amkbcj";
+                $username2="u9vdpg8vw9h2e"; $password2="f1x.A1pgN[BwX4[t"; $database2="dbpsjng5amkbcj";
                 $conn2 = new mysqli("localhost", $username2, $password2, $database2);
                 $conn2->select_db($database2) or die("Unable to select database");
 
                 //Deletes old tables
-                $SQL2 = "DROP TABLE IF EXISTS `laws".$row["ID"]."`";
+                $SQL2 = "DROP TABLE IF EXISTS `".$row["ID"]."`";
                 $conn2->query($SQL2);
 
                 //Creates new tables
-                $SQL3 = "CREATE TABLE IF NOT EXISTS `laws".$row['ID']."` (
+                $SQL3 = "CREATE TABLE IF NOT EXISTS `".$row['ID']."` (
                     `enactDate` date NOT NULL,
                     `enforceDate` date NOT NULL,
                     `lastactDate` date NOT NULL,
